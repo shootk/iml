efddc92b1e9aea2913ad48647a223dd36bbbcac2
shootk <shoot.k.uta.7@gmail.com>
2020-03-24 11:26:10 +0900

    in

--- index.js
+++ index.js
@@ -8,21 +8,43 @@ dateT = ["日", "月", "火", "水", "木", "金", "土"];
 //何周目かの初期値を入れるため，月初に設定
 var WeekNum = 0;
 document.open();
+document.write("in");
 for (var i = 1; i <= GetumatuDate; i++) {
-    today.setDate(i);
-    if (i == 1) {
-        if (dateT[today.getDay()] != "日") {
-            document.write("#" + (today.getMonth() + 1) + "月" + WeekNum + "W" + "<br>");
-            document.write("[[" + (today.getMonth() + 1) + "/" + today.getDate() + "(" + dateT[today.getDay()] + ")]]" + "<br>");
-        }
-    } else {
-
-        if (dateT[today.getDay()] == "日") {
-            WeekNum = WeekNum + 1;
-            document.write("#" + (today.getMonth() + 1) + "月" + WeekNum + "W" + "<br>");
-        }
-
-        document.write("[[" + (today.getMonth() + 1) + "/" + today.getDate() + "(" + dateT[today.getDay()] + ")]]" + "<br>");
+  today.setDate(i);
+  if (i == 1) {
+    if (dateT[today.getDay()] != "日") {
+      document.write(
+        "#" + (today.getMonth() + 1) + "月" + WeekNum + "W" + "<br>"
+      );
+      document.write(
+        "[[" +
+          (today.getMonth() + 1) +
+          "/" +
+          today.getDate() +
+          "(" +
+          dateT[today.getDay()] +
+          ")]]" +
+          "<br>"
+      );
     }
+  } else {
+    if (dateT[today.getDay()] == "日") {
+      WeekNum = WeekNum + 1;
+      document.write(
+        "#" + (today.getMonth() + 1) + "月" + WeekNum + "W" + "<br>"
+      );
+    }
+
+    document.write(
+      "[[" +
+        (today.getMonth() + 1) +
+        "/" +
+        today.getDate() +
+        "(" +
+        dateT[today.getDay()] +
+        ")]]" +
+        "<br>"
+    );
+  }
 }
 document.close();
