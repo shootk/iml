728694b48a6194ef0cfc5dcf2d2688e1b5670fa6
shootk <shoot.k.uta.7@gmail.com>
2020-04-03 17:56:36 +0900

    week change

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-  //今日の日付
-  var today = new Date();
-  //今月の月末日を今日の日付から計算．
-  var endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-  var endOfMonthDate = endOfMonth.getDate();
-  //曜日変換用配列
-  dayTable = ["日", "月", "火", "水", "木" "金", "土"];
-  //何周目かの初期値を入れるため，月初に設定
-  var weekNum = 1;
-  //書き込むドキュメントを開く
-  document.open();
-  //以下、日付と対応した書き込みを行う
-  //ループの中で毎回if文使うのはナンセンスな気がするので、月初めに確実に行う処理はループの外で記述
-  
-  today.setDate(1);
-  document.write("#" + (today.getMonth() + 1) + "月" + weekNum + "W" + "<br>");
+//今日の日付
+var today = new Date();
+//今月の月末日を今日の日付から計算．
+var endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+var endOfMonthDate = endOfMonth.getDate();
+//曜日変換用配列
+dayTable = ["日", "月", "火", "水", "木", "金", "土"];
+//何周目かの初期値を入れるため，月初に設定
+var weekNum = 1;
+//書き込むドキュメントを開く
+document.open();
+//以下、日付と対応した書き込みを行う
+//ループの中で毎回if文使うのはナンセンスな気がするので、月初めに確実に行う処理はループの外で記述
+
+today.setDate(1);
+document.write("#" + (today.getMonth() + 1) + "月" + weekNum + "W" + "<br>");
+document.write(
+  "[[" +
+    (today.getMonth() + 1) +
+    "/" +
+    today.getDate() +
+    "(" +
+    dateTable[today.getDay()] +
+    ")]]" +
+    "<br>"
+);
+for (var i = 2; i <= endOfMonthDate; i++) {
+  today.setDate(i);
+  if (dateTable[today.getDay()] == "日") {
+    weekNum = weekNum + 1;
+    document.write(
+      "#" + (today.getMonth() + 1) + "月" + weekNum + "W" + "<br>"
+    );
+  }
+
   document.write(
     "[[" +
       (today.getMonth() + 1) +
@@ -24,24 +43,5 @@
       ")]]" +
       "<br>"
   );
-  for (var i = 2; i <= endOfMonthDate; i++) {
-    today.setDate(i);
-    if (dateTable[today.getDay()] == "日") {
-      weekNum = weekNum + 1;
-      document.write(
-        "#" + (today.getMonth() + 1) + "月" + weekNum + "W" + "<br>"
-      );
-    }
-  
-    document.write(
-      "[[" +
-        (today.getMonth() + 1) +
-        "/" +
-        today.getDate() +
-        "(" +
-        dateTable[today.getDay()] +
-        ")]]" +
-        "<br>"
-    );
-  }
-  document.close();
+}
+document.close();
