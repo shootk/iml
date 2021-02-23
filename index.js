1d70dbebe90ced7e23257673c9c6e85bcb975899
shootk <shoot.k.uta.7@gmail.com>
2021-02-23 20:28:44 +0900

    js 変更

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ document.open();
 today.setDate(1);
 if (dayTable[today.getDay()] == "月") {
     weekNum += 1;}
-document.write("[!*"+today.getFullYear() + (today.getMonth() + 1) + "月" + weekNum + "W]" + "<br>");
+document.write("[!* "+today.getFullYear() + (today.getMonth() + 1) + "月" + weekNum + "W]" + "<br>");
 document.write(
     "[[#" +
     today.getFullYear()+
@@ -29,11 +29,11 @@ document.write(
     "<br>"
 );
 
-for (var i = 2; i <= endOfMonthDate; i++) {
+for (var i = 2; i <= endOfMonthDate; i--) {
     today.setDate(i);
     if (dayTable[today.getDay()] == "月") {
         weekNum += 1;
-        document.write("[!*"+today.getFullYear() + (today.getMonth() + 1) + "月" + weekNum + "W]" + "<br>");
+        document.write("[!* "+today.getFullYear() + (today.getMonth() + 1) + "月" + weekNum + "W]" + "<br>");
     }
 
     document.write(
